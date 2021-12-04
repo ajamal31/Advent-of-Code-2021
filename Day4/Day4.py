f6daec235740117f1fdd16049f7fab22857156fd
Arun Jamal <ajamal31@hotmail.com>
2021-12-04 00:11:23 -0700

    Update Day4.py

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -2,7 +2,7 @@ import requests
 from collections import defaultdict
 
 # Make sure to check that the cookie is still valid
-cookie = '53616c7465645f5f4852c18401ca934d9e2f5fc81011f8929d0adfa1f55408c117830a916fcb36e38722b641344548d3'
+cookie = '<session ID here>'
 headers = {'session': cookie}
 day = 4  # Update the day as needed
 url = f'https://adventofcode.com/2021/day/{day}/input'
