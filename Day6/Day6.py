68d5de5f2fc3f54879261779c7f02dea143f2e3a
Arun Jamal <ajamal31@hotmail.com>
2021-12-07 16:19:23 -0700

    Day 6 Advent of Code

---
+++ Day6/Day6.py
@@ -0,0 +1,41 @@
+from typing import Coroutine
+import requests
+from collections import defaultdict
+
+# Make sure to check that the cookie is still valid
+cookie = '53616c7465645f5f4852c18401ca934d9e2f5fc81011f8929d0adfa1f55408c117830a916fcb36e38722b641344548d3'
+headers = {'session': cookie}
+day = 6  # Update the day as needed
+url = f'https://adventofcode.com/2021/day/{day}/input'
+session = requests.Session()
+resp = session.get(url, cookies=headers)
+lines = resp.text
+# lines.pop()
+
+# Use this section for the sample input
+# lines = """3,4,3,1,2"""
+# lines = lines.split('\n')
+# Sample input --------------------------
+
+counts = defaultdict(int)
+
+data = [*map(int, lines.split(','))]
+counts = defaultdict(int)
+
+for i in range(9):
+    counts[i] = 0
+
+for num in data:
+    counts[num] += 1
+
+for j in range(256):
+    cur_counts = list(counts.values())
+    for i in range(0, 9):
+        if i == 8:
+            counts[8] = cur_counts[0]
+        else:
+            counts[i] = cur_counts[i + 1]
+            if i == 6:
+                counts[6] += cur_counts[0]
+
+print(sum(counts.values()))
